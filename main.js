0475a14d252a613b68932893030930bedca6f229
pook mini <mindhox@proton.me>
2024-07-29 17:56:05 +0200

    first

---
+++ main.js
@@ -0,0 +1,53 @@
+import "./style.css";
+
+// L'URL de l'API, assurez-vous que le serveur est démarré et écoute sur le port 11434
+const apiUrl = "http://localhost:11434/api/chat";
+
+const responseDiv = document.getElementById("response");
+
+const start = document.getElementById("start");
+
+start.addEventListener("click", function (event) {
+  event.preventDefault();
+  console.log("Sending:", text);
+  fetchData();
+});
+
+// La requête fetch
+function fetchData() {
+  // Les données à envoyer avec la requête
+  const requestData = {
+    model: "qwen2:0.5b",
+    messages: [
+      {
+        role: "user",
+        content: document.getElementById("text").value,
+      },
+    ],
+    stream: false,
+  };
+
+  console.log("Requesting:", requestData);
+  fetch(apiUrl, {
+    method: "POST", // Type de requête
+    headers: {
+      "Content-Type": "application/json", // Indique que le contenu est en JSON
+    },
+    body: JSON.stringify(requestData), // Convertit les données en chaîne JSON
+  })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Network response was not ok " + response.statusText);
+      }
+      return response.json(); // Traite la réponse en tant que JSON
+    })
+    .then((data) => {
+      // console.log("Response:", data); // Affiche les données de réponse
+      // console.log(data.message.content);
+      const response = data.message.content;
+      responseDiv.innerHTML = response;
+    })
+    .catch((error) => {
+      console.error("There was a problem with the fetch operation:", error);
+    });
+}
